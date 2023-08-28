94e4635fc929cd235c9520cacde98f7e6d3e3e27
dazao <2995332268@qq.com>
2023-08-28 21:40:39 +0800

    创建cloud项目

---
+++ src/test/java/com/dazao/cloudstudy/CloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazao.cloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
