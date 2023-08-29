ed15d82bc32f4ec9c7d292160ed76d1d5f9f0203
dazao <2995332268@qq.com>
2023-08-29 18:09:56 +0800

    配置完成cloud项目基本框架

--- borrowService/src/main/java/com/borrow/controller/BorrowController.java
+++ borrowService/src/main/java/com/borrow/controller/BorrowController.java
@@ -8,7 +8,6 @@ import com.dazao.User;
 import dto.BorrowUserBookDto;
 import finalconst.OtherServiceUrl;
 import jakarta.annotation.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
